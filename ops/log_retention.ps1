a084c111dd43282bad63888805a0b14f83fc7d82
domquintal <dominikquintal@gmail.com>
2025-09-12 16:31:42 -0400

    Operion patch: PIN gate + shortcut + log retention @ 2025-09-12 16:31:42

---
+++ ops/log_retention.ps1
@@ -0,0 +1,6 @@
+﻿$ErrorActionPreference = 'Stop'
+$Logs = 'C:\\Users\\Domin\\Operion\\_logs'
+if (!(Test-Path $Logs)) { exit 0 }
+Get-ChildItem -Path $Logs -File -Recurse -ErrorAction SilentlyContinue |
+  Where-Object { $_.LastWriteTime -lt (Get-Date).AddDays(-30) } |
+  Remove-Item -Force -ErrorAction SilentlyContinue
